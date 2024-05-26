4d1796e41bf7777bf417edb51d9b1f5ebfd98b6b
Hanrickson E. Dumapit <hanz.dumapit53@gmail.com>
2024-05-27 00:02:04 +0800

    db

--- database/migrations/2024_05_25_132942_create_persons.php
+++ database/migrations/2024_05_25_132942_create_persons.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         DB::statement('CREATE TABLE persons(
             id INT PRIMARY KEY AUTO_INCREMENT,
-            person_type INT NOT NULL,
+            person_type_id INT NOT NULL,
+            brgy_id INT NOT NULL,
             first_name varchar(100) NOT NULL,
             middle_name varchar(100) DEFAULT NULL,
             last_name varchar(100) NOT NULL,
@@ -30,9 +31,9 @@ return new class extends Migration
         DB::statement('CREATE INDEX idx_persons_contact_number ON persons(contact_number(10));');
         DB::statement('CREATE INDEX idx_persons_img_url ON persons(img_url(10));');
         DB::statement('CREATE INDEX idx_persons_fullname ON persons(first_name(10),middle_name(10),last_name(10));');
-        
-        DB::statement('INSERT INTO `persons`(`id`, `person_type`, `first_name`, `middle_name`, `last_name`, `suffix`, `contact_number`, `email`, `img_url`, `is_active`, `date_created`, `date_updated`)
-VALUES (NULL,1,"Admin","","Trator",NULL,"09265827342","drusha01@gmail.com","default.png",1,NOW(),NOW());');
+
+        DB::statement('INSERT INTO `persons` VALUES 
+        (NULL,1,34717,"Admin","","Trator",NULL,"09265827342","drusha01@gmail.com","default.png",1,NOW(),NOW());');
 
     }
 
