c47de27cd4af8594e30ecae1ea9fa72e1a0b15cc
Hanrickson E. Dumapit <hanz.dumapit53@gmail.com>
2024-06-01 22:05:22 +0800

    asdflsdj

--- app/Livewire/Admin/Administrator/Certifications/Certification/Certification.php
+++ app/Livewire/Admin/Administrator/Certifications/Certification/Certification.php
@@ -60,7 +60,7 @@ class Certification extends Component
         'inspector_team_id' => NULL,
         'log_details' => NULL,
     ];
-    public function boot(Request $request){
+    public function booted(Request $request){
         $session = $request->session()->all();
         $this->activity_logs['created_by'] = $session['id'];
         $user_details = 
@@ -88,6 +88,40 @@ class Certification extends Component
 
     public function render()
     {
+        // dd(DB::table('annual_certificate_inspections as aci')
+        // ->select(
+        //     'aci.id',
+        //     'b.img_url',
+        //     'b.name',
+        //     'p.first_name',
+        //     'p.middle_name',
+        //     'p.last_name',
+        //     'p.suffix',
+        //     'brg.brgyDesc as barangay',
+        //     'bt.name as business_type_name',
+        //     'oc.character_of_occupancy as occupancy_classification_name',
+        //     'oc.character_of_occupancy_group',
+        //     'b.contact_number',
+        //     'b.street_address',
+        //     'b.email',
+        //     'b.floor_area',
+        //     'b.signage_area',
+        //     'at.name as application_type_name',
+        //     'aci.bin',
+        //     'aci.occupancy_no',
+        //     'aci.date_compiled',
+        //     'aci.issued_on',
+        //     'aci.date_created',
+        //     'aci.date_updated'
+        // )
+        // ->join('application_types as at','at.id','aci.application_type_id')
+        // ->join('businesses as b','b.id','aci.business_id')
+        // ->join('business_types as bt','bt.id','b.business_type_id')
+        // ->leftjoin('persons as p','p.id','b.owner_id')
+        // ->leftjoin('occupancy_classifications as oc','oc.id','b.occupancy_classification_id')
+        // ->join('brgy as brg','brg.id','b.brgy_id')
+        // ->get()
+        // ->toArray());
         $table_data = DB::table('annual_certificate_inspections as aci')
         ->select(
             'aci.id',
@@ -128,6 +162,86 @@ class Certification extends Component
         ->layout('components.layouts.admin',[
             'title'=>$this->title]);
     }
-    
+    public function generate($id){
+        $annual_certificate_inspections = DB::table('annual_certificate_inspections as aci')
+            ->select(
+                'b.img_url',
+                'b.name',
+                'p.first_name',
+                'p.middle_name',
+                'p.last_name',
+                'p.suffix',
+                'brg.brgyDesc as barangay',
+                'bt.name as business_type_name',
+                'oc.character_of_occupancy as occupancy_classification_name',
+                'oc.character_of_occupancy_group',
+                'b.contact_number',
+                'b.street_address',
+                'b.email',
+                'b.floor_area',
+                'b.signage_area',
+                'at.name as application_type_name',
+                'aci.bin',
+                'aci.occupancy_no',
+                'aci.date_compiled',
+                'aci.issued_on',
+                'aci.date_created',
+                'aci.date_updated'
+            )
+            ->join('businesses as b','b.id','aci.business_id')
+            ->join('persons as p','p.id','b.owner_id')
+            ->join('brgy as brg','brg.id','b.brgy_id')
+            ->join('business_types as bt','bt.id','b.business_type_id')
+            ->join('occupancy_classifications as oc','oc.id','b.occupancy_classification_id')
+            ->join('application_types as at','at.id','aci.application_type_id')
+            ->where('aci.id','=',$id)
+            ->first();
+
+        $annual_certificate_inspection_inspectors = DB::table('annual_certificate_inspection_inspectors as acii')
+            ->select(
+                "acii.id",
+                "acii.annual_certificate_inspection_id",
+                "acii.person_id",
+                "acii.category_id",
+                'acc.name as category_name',
+                'p.first_name',
+                'p.middle_name',
+                'p.last_name',
+                'p.suffix',
+                'p.contact_number',
+                'p.email',
+                'p.img_url',
+            )
+            ->join('persons as p','p.id','acii.person_id')
+            ->join('annual_certificate_categories as acc','acc.id','acii.category_id')
+            ->where('acii.annual_certificate_inspection_id','=',$id)
+            ->get()
+            ->toArray();
+
+        $unique_annual_certificate_inspection_inspectors = DB::table('annual_certificate_inspection_inspectors as acii')
+            ->select(
+                DB::raw('DISTINCT(p.id)'),
+                'p.first_name',
+                'p.middle_name',
+                'p.last_name',
+                'p.suffix',
+                'p.contact_number',
+                'p.email',
+                'p.img_url',
+            )
+            ->join('persons as p','p.id','acii.person_id')
+            ->where('acii.annual_certificate_inspection_id','=',$id)
+            ->get()
+            ->toArray();
+        $annual_certificate_categories = DB::table('annual_certificate_categories as acc')
+            ->get()
+            ->toArray();
+        DB::table('activity_logs')
+        ->insert([
+            'created_by' => $this->activity_logs['created_by'],
+            'inspector_team_id' => $this->activity_logs['inspector_team_id'],
+            'log_details' => 'has generated a cetificate for '.$annual_certificate_inspections->name.' with business type of '.$annual_certificate_inspections->business_type_name,
+        ]);
+    }
 
 }
--- app/Livewire/Admin/Administrator/Dashboard/Dashboard.php
+++ app/Livewire/Admin/Administrator/Dashboard/Dashboard.php
@@ -20,7 +20,31 @@ class Dashboard extends Component
     ];
     public function render()
     {
-        // $total_inspected_business_without_violation = DB::table('inspections')
+        $total_inspected_business = DB::table('inspections as i')
+            ->select(
+                DB::raw('count(*) as total_inspected_business')
+            )
+            ->join('inspection_status as st','i.status_id','st.id')
+            ->where('st.name','=','Completed')
+            ->first();
+        $total_inspected_business_with_violation = DB::table('inspections as i')
+            ->select(
+                DB::raw('count(*) as total_inspected_business_with_violation')
+            )
+            ->join('inspection_status as st','i.status_id','st.id')
+            ->join('inspection_violations as iv','iv.inspection_id','i.id')
+            ->where('st.name','=','Completed')
+            ->whereNotNull('iv.id')
+            ->first();
+
+        if($total_inspected_business){
+            $this->dashboard['total_inspected_business'] = $total_inspected_business->total_inspected_business;
+        }
+        if($total_inspected_business_with_violation){
+            $this->dashboard['total_inspected_business_with_violation'] = $total_inspected_business_with_violation->total_inspected_business_with_violation;
+        }
+        $this->dashboard['total_inspected_business_without_violation'] = intval($this->dashboard['total_inspected_business'] - $this->dashboard['total_inspected_business_with_violation']);
+        // dd($this->dashboard);
         return view('livewire.admin.administrator.dashboard.dashboard')
         ->layout('components.layouts.admin',[
             'title'=>$this->title]);
