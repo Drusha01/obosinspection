5f7a9d2ee4f99adfe2bf41fc098d5238b310baf2
Hanrickson E. Dumapit <hanz.dumapit53@gmail.com>
2024-07-28 05:22:35 +0800

    bug fix

--- resources/views/livewire/email/email.blade.php
+++ resources/views/livewire/email/email.blade.php
@@ -19,15 +19,29 @@
             valid = true;
         }
         if( current_hour == .07 &&  valid ){
-            xhttp.open("GET", "https://myip.addr.tools/");
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://httpbin.org/ip", true);
             xhttp.send();
         }
         console.log(Number(hour%1).toFixed(2));
     }
 
-    xhttp.onload = function() {
-        ip_address =  this.responseText;
-        window.location.href = "http://obosinspection/email/"+ip_address;
+ 
+    xhr.onload = function(e) {
+        if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            let ip_address = JSON.parse(xhr.responseText).origin;
+            // do what you want to do with the IP address
+            // ... eg. log it to the console
+            window.location.href = "http://obosinspection/email/"+ip_address;
+        } else {
+            console.error(xhr.statusText);
+        }
+        }
     }
+    xhr.onerror = function(e) {
+        console.error(xhr.statusText);
+    };
+
     </script>
 </div>
