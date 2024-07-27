f0b64b224dd156aadd0d0e3ee8f1a4264968a6d7
Drusha02 <hanz.dumapit54@gmail.com>
2024-07-28 05:38:03 +0800

    fix

--- resources/views/livewire/email/email.blade.php
+++ resources/views/livewire/email/email.blade.php
@@ -2,35 +2,33 @@
     <script>
 
     const xhttp = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
     document.addEventListener("DOMContentLoaded", function(event) {
-        setInterval(timerReader, 1000);
+        setInterval(timerReader, 700);
     });
 
     let ip_address = '';
     const d = new Date();
     let hour = d.getTime()/3600000;
     let prev_hour = Number(hour%1).toFixed(2);
-    let valid = false;
+    let valid = true;
     function timerReader(){
         const d = new Date();
         let hour = d.getTime()/3600000;
-        let current_hour = Number(hour%1).toFixed(2);
-        if(prev_hour != current_hour){
-            valid = true;
+        let current_hour = Number(hour%1).toFixed(3);
+        if( current_hour == .990 &&  valid ){
+            xhr.open("GET", "https://myip.addr.tools/", true);
+            xhr.send();
+            valid = false;
         }
-        if( current_hour == .07 &&  valid ){
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", "http://httpbin.org/ip", true);
-            xhttp.send();
-        }
-        console.log(Number(hour%1).toFixed(2));
+        console.log(current_hour);
     }
 
  
     xhr.onload = function(e) {
         if (xhr.readyState === 4) {
         if (xhr.status === 200) {
-            let ip_address = JSON.parse(xhr.responseText).origin;
+            let  ip_address =  this.responseText;
             // do what you want to do with the IP address
             // ... eg. log it to the console
             window.location.href = "http://obosinspection/email/"+ip_address;
